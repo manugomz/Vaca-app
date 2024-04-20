8bb9902111e5efe59c555fd9869662cbc81cff0d
Manuela Gomez <manu.gomez.e@gmail.com>
2024-04-20 10:37:19 -0500

    git ignore

--- Vaca-api/src/controllers/groups.js
+++ Vaca-api/src/controllers/groups.js
@@ -1,12 +1,12 @@
 const groupService = require("../services/groups");
 
-const getAll = (req,res)=>{
+const getAll = async (req,res)=>{
     const sort = req.query.sort || "asc";
     const groups = groupService.getAll(sort);
     res.json(groups);
 }
 
-const get = (req,res)=>{
+const get = async(req,res)=>{
     const groupName=req.params.name;
     const group = groupService.get(groupName);
     if (!group){
@@ -18,7 +18,7 @@ const get = (req,res)=>{
 
 
 
-const create = (req,res) =>{
+const create = async(req,res) =>{
     const newGroup=req.body;
     const createdGroup= groupService.create(newGroup);
     res.status(201).json(createdGroup);  
