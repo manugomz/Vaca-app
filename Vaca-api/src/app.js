77d2012624629eb5af060ccd844e3843cf6989b8
Manuela Gómez E <manu.gomez.e@gmail.com>
2024-07-04 16:10:30 -0500

    move database middleware

--- Vaca-api/src/app.js
+++ Vaca-api/src/app.js
@@ -5,6 +5,8 @@ import express from 'express';
 import passport from 'passport';
 
 import mainRouter from './routes/async.router.js';
+import { connectDatabase, commitDatabase, rollbackDatabase } from './lib/database.middleware.js';
+
 import './utils/passport.config.js';
 
 //config
@@ -13,10 +15,11 @@ const PORT = process.env.process || 3000;
 
 app.use(express.json());
 app.use(cors());
-
-app.use(mainRouter());
+app.use(connectDatabase);
 app.use(passport.initialize());
-
+app.use(mainRouter());
+app.use(commitDatabase);
+app.use(rollbackDatabase);
 
 app.listen(PORT, () => {
     console.info(`Express server runing at http://localhost:${PORT}`);
--- Vaca-api/src/routes/async.router.js
+++ Vaca-api/src/routes/async.router.js
@@ -4,19 +4,14 @@ import authRouter from './auth.router.js';
 import groupsRouter from './groups.router.js';
 import usersRouter from './users.router.js';
 
-import { connectDatabase, commitDatabase, rollbackDatabase } from '../lib/database.middleware.js';
-
 //Configurar rutas
 
 const AsyncRouter = () => {
     const router = Router();
 
-    router.use(connectDatabase);
     router.use('/groups', groupsRouter());
     router.use('/users', usersRouter());
     router.use('/auth', authRouter());
-    router.use(commitDatabase);
-    router.use(rollbackDatabase);
 
     return router;
 };
